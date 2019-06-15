076549804dc802bea7152e7a68bebfdd33f72a34
Khac Tung <nguyen.khac.tung@miyatsu.vn>
2019-06-15 16:56:06 +0700

    update free json

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,41 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ANGELHACK VIETNAM HACKATHON 2019', teamName: 'Domo team' });
 });
 
-router.get('/vnpostfree/:code', function(req, res, next) {
-  // const urlCode = req.params.code;
-  var result = {
-    'a': 1,
-  };
+const g_scope_code = {
+    insite: '001',
+    outsite: '002',
+};
+router.get('/vnpostfree', function(req, res, next) {
+  var body = req.body,
+      scope_code = body.scope_code,
+      add_from = body.from,
+      add_to = body.to,
+      weight = body.weight,
+      size = body.size;
+
+    var result = [
+        {
+            service_name: 'Bưu phẩm thường',
+            fee_guess: '14.000 VNĐ'
+        },
+        {
+            service_name: 'Chuyển phát nhanh EMS',
+            fee_guess: '43.700  VNĐ',
+            time: 'Từ 1.5 đến 2 ngày',
+        },
+        {
+            service_name: 'Bưu phẩm bảo đảm',
+            fee_guess: '18.500  VNĐ',
+        },
+        {
+            service_name: 'Logistics Eco',
+            fee_guess: '120.000  VNĐ',
+        },
+        {
+            service_name: 'Bưu kiện trong nước',
+            fee_guess: '17.000  VNĐ',
+        }
+    ];
   res.status(200).send(result);
 });
 
